68748fdd519d04571e35db1eca8bddcbd60c1590
Purvai <purvai1246@gmail.com>
2026-04-30 15:04:34 +0530

    style: Set light theme as default

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes';
 
 export default function App() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
       <RouterProvider router={router} />
     </ThemeProvider>
   );
